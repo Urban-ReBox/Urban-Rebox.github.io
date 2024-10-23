c5f947ed8e4dc52fea1081129ae3b97ca26dad60
Quentin Horgues <quentin.horgues@outlook.fr>
2024-10-23 23:54:42 +0200

    correction orth

--- src/components/Staff/index.js
+++ src/components/Staff/index.js
@@ -9,37 +9,37 @@ const MemberList = [
     },
     {
       name: "Sophie Raquin",
-      role: "Résponsable sociétale de l'entreprise",
+      role: "Responsable sociétale de l'entreprise",
       profil_icon: "/img/members/sophie.jpg"
     },
     {
       name: "Quentin Horgues",
-      role: "Résponsable communication",
+      role: "Responsable communication",
       profil_icon: "/img/members/quentin.jpg"
     },
     {
       name: "Dario Weinberger",
-      role: "Résponsable communication",
+      role: "Responsable communication",
       profil_icon: "/img/members/dario.jpg" 
     },
     {
       name: "Vincent Shurgaia",
-      role: "Résponsable preuve de concepte",
+      role: "Responsable preuve de concept",
       profil_icon: "/img/members/vincent.jpg"
     },
     {
       name: "Said Bouargalne",
-      role: "Résponsable preuve de concepte",
+      role: "Responsable preuve de concept",
       profil_icon: "/img/members/said.jpg"
     },
     {
       name: "Yoann Girard-le Plessis",
-      role: "Résponsable buisness modèle",
+      role: "Responsable buisness model",
       profil_icon: "/img/members/yoann.jpg"
     },
     {
       name: "Floryan Gosselet",
-      role: "Résponsable buisness modèle",
+      role: "Responsable buisness model",
       profil_icon: "/img/members/floryan.jpg"
     },
   ];
